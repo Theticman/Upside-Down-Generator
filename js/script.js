f1c79b7df42a72a824b82210fdee0734605c98a9
Theticman <bpoirier.341@gmail.com>
2020-07-26 17:41:53 +0200

    Updating some variable name

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Creating Constants & Variables
 const sheet_url = 'https://spreadsheets.google.com/feeds/cells/1KVlsHKtK03-C8Q6jdcGuNVAYt0lSrLdORgjLs6dkvBc/od6/public/basic?alt=json';
 var input_text = '';
+console.log("TEST");
 
-
+// Functions
 async function getdata() {
     console.log("Calling API...");
     const response = await fetch(sheet_url);
     var raw_data = await response.json();
     raw_data = raw_data.feed.entry;
     console.log("Raw data pulled:",raw_data);
-
     var table = [[],[]];
     var i = 0;
     console.log("Reformating to usable JSON...");
@@ -24,25 +24,31 @@ async function getdata() {
     console.log("Final table",table);
 }
 
-function get_raw_text() {
-    return document.getElementById('raw_text').value
+function get_input_text() {
+    return document.getElementById('input_text').value
+}
+
+function display_ud_text(text) {
+    document.getElementById('upside_down_text').value = text;
 }
 
 function generate_init() {
-    if (input_text != get_raw_text()) {
+    if (input_text != get_input_text()) {
         generate();
     }
 }
 
 function generate() {
-    var raw_text = get_raw_text();
-    input_text = '';
+    console.log("Generating new output...");
+    input_text = get_input_text();
+    var upside_down = '';
     var i = 1;
-    for (i = 1; i <= raw_text.length; i++) {
-        const element = raw_text[raw_text.length-i];
-        input_text = input_text+element;
+    for (i = 1; i <= input_text.length; i++) {
+        const element = input_text[input_text.length-i];
+        upside_down = upside_down+element;
     }
-    document.getElementById('upside_down_text').value = input_text;
+    display_ud_text(upside_down);
 }
 
+// Ticking (loop to check for update every 100ms)
 setInterval(generate_init, 100)

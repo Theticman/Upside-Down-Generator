2c1fdff924c19ed3beb16eda072c05c931ae72b5
Baptiste POIRIER <bpoirier.341@gmail.com>
2020-08-04 13:54:47 +0200

    More refined code

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ function copy_to_clipboard(text_id) {
     var text = document.getElementById(text_id).value
     console.log("Copy to clipboard:",text);
     window.navigator.clipboard.writeText(text);
+    document.getElementById("tooltip").innerHTML = "Copied!";
 }
 
 function get_input_text() {
@@ -97,6 +98,7 @@ function convert_text_parts(text_parts) {
 
 function generate() {
     console.log("Start generate...");
+    document.getElementById("tooltip").innerHTML = "Click to copy"
     fetch('https://api.countapi.xyz/update/theticman/testing/?amount=1');
     var upside_down = '';
     text_parts = [];
